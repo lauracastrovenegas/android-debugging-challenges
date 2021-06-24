4421bafdd3ecd66aee606dfe58a59c73342d9940
Laura Castro Venegas <laurasofi1024@gmail.com>
2021-06-24 12:03:21 -0400

    fixed day and background changer

--- app/src/main/java/com/codepath/debuggingchallenges/activities/ChangeBackgroundActivity.java
+++ app/src/main/java/com/codepath/debuggingchallenges/activities/ChangeBackgroundActivity.java
@@ -4,17 +4,21 @@ import android.graphics.Color;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 
 import com.codepath.debuggingchallenges.R;
 
 public class ChangeBackgroundActivity extends AppCompatActivity {
 
     private int oldColor = Color.BLUE;
+    Button btnGo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_change_background);
+
+        btnGo = (Button) findViewById(R.id.btnGo);
     }
 
     public void onGoClick(View view) {
